9c3a8432922ae324dae114e804f8f69e6ac24662
Kevin Yan <k3vinyan@gmail.com>
2016-06-26 06:04:44 -0700

    add user authentication

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    end
+    return
+  end
+
+  def destroy
+    puts "hello"
   end
 end
